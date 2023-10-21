8e16f1630cda7850b306bb78a11638fd95f8bac2
TatyyanaS <taty.yana.kz@gmail.com>
2023-10-21 19:12:53 +0600

    finally done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let brackets = new Map(bracketsConfig);
+  let sameBrackets = new Set();
+
+  for (let [open, close] of bracketsConfig) {
+      if (open === close) {
+          sameBrackets.add(open);
+      }
+  }
+
+  for (let char of str) {
+      if (brackets.has(char)) {
+          if (sameBrackets.has(char) && stack[stack.length - 1] === char) {
+              stack.pop();
+          } else {
+              stack.push(char);
+          }
+      } else {
+          if (stack.length === 0 || brackets.get(stack.pop()) !== char) {
+              return false;
+          }
+      }
+  }
+
+  return stack.length === 0;
 }
